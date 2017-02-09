4deaa03e2fd822acced86edbef2df34d00aa54e9
johnnyjong823 <johnnyjong823@gmail.com>
2017-02-09 15:04:41 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
+
 // you may not use this file except in compliance with the License.
+
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// 
+Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+//
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+ See the License for the specific language governing permissions and
+
 // limitations under the License.
+
 
 package main
 
@@ -49,7 +56,8 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do(); err != nil {
+				var text = "§A¦n"
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(text +":"+message.Text+" OK!")).Do(); err != nil {
 					log.Print(err)
 				}
 			}
